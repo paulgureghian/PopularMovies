032795bc15d2e52fc47c8fce8ba7c4a1cf73dd16
Paul <paulgureghian@att.net>
2016-05-11 17:39:17 -0700

    5-11-16  5:30 PM

--- app/src/main/java/com/example/android/popularmoviesdemo/TopRatedEndpoint.java
+++ app/src/main/java/com/example/android/popularmoviesdemo/TopRatedEndpoint.java
@@ -4,11 +4,10 @@ package com.example.android.popularmoviesdemo;
 import retrofit.Callback;
 import retrofit.http.GET;
 
-public interface MoviesApiService {
-    @GET("/movie/popular")
+public interface TopRatedEndpoint {
 
+    @GET("/movie/top_rated ")
     void getPopularMovies(Callback<Movie.MovieResult> cb);
+
+
 }
-
-
-

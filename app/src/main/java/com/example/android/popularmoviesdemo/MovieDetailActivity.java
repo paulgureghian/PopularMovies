cd58f803f19efa03d4cf333f3bc1eda990be913c
Paul <paulgureghian@att.net>
2016-05-19 17:27:51 -0700

    5-19-16  5:30 PM

--- app/src/main/java/com/example/android/popularmoviesdemo/MovieDetailActivity.java
+++ app/src/main/java/com/example/android/popularmoviesdemo/MovieDetailActivity.java
@@ -1,11 +1,15 @@
 package com.example.android.popularmoviesdemo;
 
+import android.content.ClipData;
 import android.os.Bundle;
 import android.support.design.*;
 import android.support.design.BuildConfig;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -30,21 +34,16 @@ public class MovieDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_movie_detail);
         if (getIntent().hasExtra(EXTRA_MOVIE)) {
             mMovie = getIntent().getParcelableExtra(EXTRA_MOVIE);
-
         } else {
             throw new IllegalArgumentException("Detail activity must receive a movie parcelable");
-
         }
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        average = (TextView)findViewById(R.id.vote_average);
+        average = (TextView) findViewById(R.id.vote_average);
         date = (TextView) findViewById(R.id.release_date);
         title = (TextView) findViewById(R.id.movie_title);
         description = (TextView) findViewById(R.id.movie_description);
         poster = (ImageView) findViewById(R.id.movie_poster);
-
-
         average.setText(mMovie.getAverage());
         date.setText(mMovie.getDate());
         title.setText(mMovie.getTitle());
@@ -52,6 +51,49 @@ public class MovieDetailActivity extends AppCompatActivity {
         Picasso.with(this)
                 .load(mMovie.getPoster())
                 .into(poster);
-          }
-}
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.Trailer) {
+            launchTrailer();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    public void launchTrailer() {
+
+        final RestAdapter restAdapter = new RestAdapter.Builder()
+                .setEndpoint("http://api.themoviemdb.org/3")
+                .setRequestInterceptor(new RequestInterceptor() {
+                    @Override
+                    public void intercept(RequestFacade request) {
+                        request.addEncodedQueryParam("api_key", BuildConfig.TMDB_API_KEY);
+                    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 

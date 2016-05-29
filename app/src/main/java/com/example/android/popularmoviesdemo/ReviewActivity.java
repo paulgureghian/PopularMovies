6ae218064eadf739d7b2b375de55fc01af5444c8
Paul <paulgureghian@att.net>
2016-05-28 17:21:34 -0700

    5-28-16  5:30  PM

--- app/src/main/java/com/example/android/popularmoviesdemo/ReviewActivity.java
+++ app/src/main/java/com/example/android/popularmoviesdemo/ReviewActivity.java
@@ -1,16 +1,25 @@
 package com.example.android.popularmoviesdemo;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ReviewActivity extends AppCompatActivity {
+    private ListView mListView;
+    public Adapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +28,21 @@ public class ReviewActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     //    setSupportActionBar(toolbar);
 
+        mListView = (ListView) findViewById(R.id.listview_review);
+        mAdapter = new Adapter(this);
+        mListView.setAdapter(mAdapter);
+        mAdapter.setReviewList(reviews);
         ArrayList<Review> reviews = getIntent().getParcelableArrayListExtra("reviews");
-
-
     }
-
+    public static class Adapter extends ListView.Adapter<Review> {
+        private List<Review> mReviewList;
+        private LayoutInflater mInflater;
+        private Context mContext;
+        public Adapter (Context context) {
+            this.mContext = context;
+            this.mInflater = LayoutInflater.from(context);
+        }
+    }
 
 
 

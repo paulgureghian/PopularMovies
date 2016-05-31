e095ce3dbf90406a7acdc0ad34f546605b6e0218
Paul <paulgureghian@att.net>
2016-05-30 17:35:07 -0700

    5-30-16  5:30 PM

---
+++ app/src/main/java/com/example/android/popularmoviesdemo/SharedPreference.java
@@ -0,0 +1,94 @@
+package com.example.android.popularmoviesdemo;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.google.gson.Gson;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SharedPreference {
+
+    public static final String PREFS_NAME = "PRODUCT_APP";
+    public static final String FAVORITES = "Product_Favorite";
+
+    public SharedPreference() {
+        super();
+    }
+
+    public void saveFavorites(Context context, List<Product> favorites) {
+        SharedPreferences settings;
+        SharedPreferences.Editor editor;
+
+        settings = context.getSharedPreferences(PREFS_NAME,
+                Context.MODE_PRIVATE);
+        editor = settings.edit();
+
+        Gson gson = new Gson();
+        String jsonFavorites = gson.toJson(favorites);
+
+        editor.putString(FAVORITES, jsonFavorites);
+        editor.commit();
+    }
+
+    public void addFavorite(Context context, Product product) {
+        List<Product> favorites = getFavorites(context);
+        if (favorites == null)
+            favorites = new ArrayList<Product>();
+        favorites.add(product);
+        saveFavorites(context, favorites);
+    }
+
+    public void removeFavorite(Context context, Product product) {
+        ArrayList<Product> favorites = getFavorites(context);
+        if (favorites != null) {
+            favorites.remove(product);
+            saveFavorites(context, favorites);
+        }
+    }
+
+    public ArrayList<Product> getFavorites(Context context) {
+        SharedPreferences settings;
+        List<Product> favorites;
+
+        settings = context.getSharedPreferences(PREFS_NAME,
+                Context.MODE_PRIVATE);
+
+        if (settings.contains(FAVORITES)) {
+            String jsonFavorites = settings.getString(FAVORITES, null);
+            Gson gson = new Gson();
+            Product[] favoriteItems = gson.fromJson(jsonFavorites,
+                    Product[].class);
+
+            favorites = Arrays.asList(favoriteItems);
+            favorites = new ArrayList<Product>(favorites);
+        } else
+            return null;
+        return (ArrayList<Product>) favorites;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

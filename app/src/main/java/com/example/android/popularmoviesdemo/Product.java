f1458356d09290a47e2eb2a071ef31efe85f8284
Paul <paulgureghian@att.net>
2016-05-31 12:51:06 -0700

    5-31-16  12:50 PM

--- app/src/main/java/com/example/android/popularmoviesdemo/Product.java
+++ app/src/main/java/com/example/android/popularmoviesdemo/Product.java
@@ -1,5 +1,44 @@
 package com.example.android.popularmoviesdemo;
 
-
 public class Product {
+
+    private int id;
+    private String name;
+    private String description;
+
+    public Product() {
+        super();
+    }
+
+    public Product(int id, String description, String name) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId() {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName() {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription() {
+        this.description = description;
+    }
+
 }

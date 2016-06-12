49f3da579e1d58efcc0b13ef1253b0cd2a6a10bf
Paul <paulgureghian@att.net>
2016-06-11 17:50:30 -0700

    6-11-16  5:50  PM

--- app/src/main/java/com/example/android/popularmoviesdemo/MainActivity.java
+++ app/src/main/java/com/example/android/popularmoviesdemo/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_detail);
 
         if (findViewById(R.id.movie_detail_container) != null) {
             mTwoPane = true;
